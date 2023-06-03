f51b35ba97912910bd8a6735bd37abf0b5b34720
dhairya-07 <dhairya3208@gmail.com>
2023-06-03 14:27:04 +0530

    Crawling started

---
+++ main.js
@@ -0,0 +1,20 @@
+const { crawlPage } = require('./crawl');
+const { printReport } = require('./report');
+
+async function main() {
+  if (process.argv.length > 3) {
+    console.log('Too many command line args');
+    process.exit(1);
+  }
+
+  if (process.argv.length < 3) {
+    console.log('No url provided');
+    process.exit(1);
+  }
+  const baseURL = process.argv[2];
+  console.log(`Starting crawl of ${baseURL}`);
+  const pages = await crawlPage(baseURL, baseURL, {});
+  printReport(pages);
+}
+
+main();
